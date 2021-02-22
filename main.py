887e0102b958c2d4e21aaa4782a635c588f8369c
LauraJin8 <Laura.jin@ocsbstudent.ca>
2021-02-22 19:15:20 +0000

    got rid of spaces

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 # Imports math library
-import math 
+import math
 
-print ("Finding the circumference (C) and the area (A) of a circle using the diameter and pi (3.14)")
+print("Finding the circumference (C) and the area (A) of a circle using the diameter and pi (3.14)")
 
 # Stores the number that the user enteres in the variable 'diameter'
-diameter = input ("Insert and enter a positive number or decimal to be the diameter of a circle: ")
+diameter = input("Insert and enter a positive number or decimal to be the diameter of a circle: ")
 
 # Makes a variable 'pi' by defining it as math.pi and this uses the math library above. Then it turns the variable 'diameter' and 'pi' into decimal numbers
 pi = math.pi
-diameter = float (diameter)
-pi = float (pi)
+diameter = float(diameter)
+pi = float(pi)
 
 # Use the print function to show the equation on the console and do the calculations for the area and circumference
-print ("A = πr^2 =", (diameter / 2) ** 2 * pi, "units^2")
-print ("C = πd =", diameter * pi, "units")
+print("A = πr^2 =", (diameter / 2) ** 2 * pi, "units^2")
+print("C = πd =", diameter * pi, "units")
